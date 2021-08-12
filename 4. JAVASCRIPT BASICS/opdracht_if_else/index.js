bba3d000a389a01295cd32343f02860b001d7b5e
Ester Engelhard <82372706+AngelhardHub@users.noreply.github.com>
2021-08-12 14:47:43 +0200

    Add files via upload

---
+++ 4. JAVASCRIPT BASICS/opdracht_if_else/index.js
@@ -0,0 +1,21 @@
+let age = 19;
+
+if(age >= 18){
+    console.log("18+, access granted");
+} else {
+    console.log("underaged, access denied");
+}
+
+let isFemale = false;
+if(isFemale){
+    console.log("lady like, access granted")
+} else {
+    console.log("man oh man, access denied")
+}
+
+let driverStatus = "bob";
+if(driverStatus = "bob") {
+    console.log("= bob, drives home");
+} else{
+    console.log("no bob, no drive");
+}
