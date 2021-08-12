2db9bed4aad81dd8b92de457f21ed6e56e4cc705
Ester Engelhard <82372706+AngelhardHub@users.noreply.github.com>
2021-08-12 12:17:28 +0200

    Add files via upload

---
+++ 4. JAVASCRIPT BASICS/Gebruik je eerste JavaScript/Opdracht-Gebruik_je_eerste_JavaScript/index.js
@@ -0,0 +1,37 @@
+console.log("Hello Winc Academy")
+
+let name = "Ester";
+console.log(name)
+
+let sum = 4 + 4
+console.log(sum)
+
+let sumSting ="4"+"4"
+console.log(sumSting)
+
+let x = 100 + 50
+console.log(x)
+
+let d = 100 / 10
+let i = 200 * (80 / 100)
+let u = d + i % 2
+console.log(u)
+
+function pythagorean(sideA, sideB) {
+  return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+}
+
+console.log(pythagorean(4, 3));
+console.log(pythagorean(10, 4));
+
+let leeftijd = 33;
+console.log(leeftijd)
+console.log(typeof leeftijd)
+
+
+let leeftijdString = "33";
+console.log(leeftijdString)
+console.log(typeof leeftijdString)
+
+
+
---
+++ 4. JAVASCRIPT BASICS/opdrachtJs2/script.js
@@ -0,0 +1,4 @@
+console.log("Hallo Winc Academy studenten")
+// Dit is een grote som, dit is niet zichtbaar in node.js, comments zijn tekst en node leest alleen code.
+// console.log(1230941 * 1823794)
+// console.log( 637263 / 54)
