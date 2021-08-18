2dd8d478caab5f09df067f0eb9f236ef86f427f6
Ester Engelhard <82372706+AngelhardHub@users.noreply.github.com>
2021-08-18 10:02:52 +0200

    Add files via upload

---
+++ 4. JAVASCRIPT BASICS/Function_basics/index.js
@@ -0,0 +1,20 @@
+const bakingPizza = function () {
+    console.log("make dough")
+    console.log("add toppings")
+    console.log("bake pizza")
+};
+
+
+const rollSushi = function (){
+    console.log("get/cook ingredients")
+    console.log("hold your nose")
+    console.log("roll sushi")
+    console.log("give away ASAP")
+}    
+
+
+bakingPizza();
+bakingPizza();
+rollSushi();
+rollSushi();
+rollSushi();
